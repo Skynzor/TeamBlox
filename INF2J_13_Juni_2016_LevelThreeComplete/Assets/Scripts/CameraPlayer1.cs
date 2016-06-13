9a71e350e2b5f647686a3e26af2779bad19d54f7
Rene Cordes <renecordes@hotmail.com>
2016-06-13 21:37:09 +0200

    LEVEL 3 COMPLETE
    Level 2 en 3 zijn complete. 1 wordt nog aan gewerkt (Animaties,
    overgangen, level complete canvas, etc) (Moet ook nog gebeuren bij Level
    2 en 3!)

---
+++ INF2J_13_Juni_2016_LevelThreeComplete/Assets/Scripts/CameraPlayer1.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraPlayer1 : MonoBehaviour {
+
+    //oude code
+    //public Transform player;
+    //public Vector3 offset;
+
+    //nieuwe code
+    private Vector2 velocity;
+
+    public float smoothTimeY;
+    public float smoothTimeX;
+
+    public GameObject player1;
+
+    public bool bounds;
+
+    public Vector3 minCameraPos;
+    public Vector3 maxCameraPos;
+
+    void Start()
+    {
+        // Oude code
+        //transform.position = new Vector3(player.position.x + offset.x, player.position.y + offset.y, offset.z); // Camera follows the player with specified offset position
+
+        //nieuwe code
+        player1 = GameObject.FindGameObjectWithTag("Player1");
+    }
+
+    void FixedUpdate()
+    {
+        float posX = Mathf.SmoothDamp(transform.position.x, player1.transform.position.x, ref velocity.x, smoothTimeX);
+        float posY = Mathf.SmoothDamp(transform.position.y, player1.transform.position.y, ref velocity.x, smoothTimeY);
+
+        transform.position = new Vector3(posX, posY, transform.position.z);
+
+        if(bounds)
+        {
+            transform.position = new Vector3(Mathf.Clamp(transform.position.x, minCameraPos.x, maxCameraPos.x),
+                Mathf.Clamp(transform.position.y, minCameraPos.y, maxCameraPos.y),
+                Mathf.Clamp(transform.position.z, minCameraPos.z, maxCameraPos.z));
+        }
+    }
+}
+
---
+++ INF2J_13_Juni_2016_LevelThreeComplete/Assets/Scripts/hitFinishLine.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class hitFinishLine : MonoBehaviour {
+
+    bool player1Arrived = false;
+    bool player2Arrived = false;
+
+	void OnTriggerEnter2D(Collider2D coll)
+    {
+        GameObject hitObj = coll.gameObject;
+
+        if(hitObj.tag == "Player1")
+        {
+            player1Arrived = true;
+        }
+
+        if (hitObj.tag == "Player2")
+        {
+            player2Arrived = true;
+        }
+
+        if (player1Arrived && player2Arrived)
+        {
+            // Level voltooid
+            Debug.Log("Beide aangekomen");
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D coll)
+    {
+        GameObject hitObj = coll.gameObject;
+
+        if (hitObj.tag == "Player1")
+        {
+            player1Arrived = false;
+            Debug.Log("Player 1 loopt weg");
+        }
+
+        if (hitObj.tag == "Player2")
+        {
+            player2Arrived = false;
+            Debug.Log("Player 2 loopt weg");
+        }
+    }
+}
