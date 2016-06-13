9170f75f4f1194c04bd53ad798d80902552035bc
Rene Cordes <renecordes@hotmail.com>
2016-06-13 10:52:49 +0200

    Level 1 complete
    Platform werkt nog steeds niet, misschien weghalen dat het onder parent
    valt.

---
+++ INF2J_13_Juni_2016_LevelOneComplete/Assets/Scripts/LevelSelectMenu.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelSelectMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    //kan terug naar hoofdmenu, maar ook spel?
+    public void Back()
+    {
+        //terug naar hoofdmenu scene
+        Application.LoadLevel(0);
+    }
+
+    //start level 1
+    public void PlayLevelOne()
+    {
+        Application.LoadLevel(1);
+        SoundManager.soundInstance.musicSource.Stop();
+        Destroy(SoundManager.soundInstance);
+
+        Score.startLevel(60);
+    }
+
+    //start level 1
+    public void PlayLevelTwo()
+    {
+        Application.LoadLevel(1);
+    }
+
+    //start level 1
+    public void PlayLevelThree()
+    {
+        Application.LoadLevel(1);
+    }
+}
