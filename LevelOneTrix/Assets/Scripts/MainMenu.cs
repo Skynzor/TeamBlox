9896fcc3f1a8318e43b0436ac70c45870e859540
Trix Beemster <trixbeemster@gmail.com>
2016-06-13 21:27:53 +0200

    LevelOneTrix met finish tekst

---
+++ LevelOneTrix/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void Play()
+    {
+        //levelselect scene
+        Application.LoadLevel(4);
+    }
+
+    public void HighScore()
+    {
+        //highscore scene
+        Application.LoadLevel(2);
+    }
+
+    public void Options()
+    {
+        //options scene
+        Application.LoadLevel(3);
+    }
+
+    public void Exit()
+    {
+        //Sluit app
+        print("Shutting down...");
+        Application.Quit();
+    }
+}
