f274d84ad598bdeea7b581e54ecb7c849c57e480
Xalandra <xalandra@gmail.com>
2016-06-14 21:08:04 +0200

    OptionsMenu added
    Resolution, windowed, sound on and off

---
+++ INF2J_14_JUNE_FINAL_BUILD_3_1/finalbuild3.1/INF2J_14_juni_FINAL_BUILD 3 AUDIO/Assets/Scripts/GroundCheck.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GroundCheck : MonoBehaviour {
+
+    private Player1 player1;
+
+    void Start ()
+    {
+        player1 = gameObject.GetComponentInParent<Player1>();
+    }
+
+    void OnTriggerStay2D(Collider2D col)
+    {
+        player1.grounded = true;
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        player1.grounded = true;
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        player1.grounded = false;
+    }
+}
