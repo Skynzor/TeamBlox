4d6b261d96de24f0e8ff9d3bd3eed58d20828ce5
OBoers <o.boers@gmail.com>
2016-06-14 17:31:52 +0200

    FINAL BUILD 3 AUDIO
    Audio added to final build 3

---
+++ INF2J_14_juni_FINAL_BUILD 3 AUDIO/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+    //Sound effects
+    public AudioClip buttonSound1;
+    public AudioClip buttonSound2;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void Play()
+    {
+        //levelselect scene
+        SoundManager.soundInstance.RandomizeSfx(buttonSound1, buttonSound2);
+        Application.LoadLevel(4);
+
+    }
+
+    public void HighScore()
+    {
+        //highscore scene
+        SoundManager.soundInstance.RandomizeSfx(buttonSound1, buttonSound2);
+        Application.LoadLevel(2);
+    }
+
+    public void Options()
+    {
+        //options scene
+        SoundManager.soundInstance.RandomizeSfx(buttonSound1, buttonSound2);
+        Application.LoadLevel(3);
+    }
+
+    public void Exit()
+    {
+        //Sluit app
+        SoundManager.soundInstance.RandomizeSfx(buttonSound1, buttonSound2);
+        print("Shutting down...");
+        Application.Quit();
+    }
+}
---
+++ INF2J_14_juni_FINAL_BUILD 3 AUDIO/Assets/Scripts/OptionsMenu.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class OptionsMenu : MonoBehaviour
+{
+    //Voor dropdown
+
+    //Sound effects
+    public AudioClip buttonSound1;
+    public AudioClip buttonSound2;
+
+    //Zet de bool playAudio in SoundManager to false om audio uit te zetten. True om het weer aan te zetten.
+    //SoundManager.soundInstance.musicSource.Stop();
+  
+
+    // Use this for initialization
+    void Start()
+    {
+        //Voor dropdown
+    }
+
+    //Voor dropdown
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    //kan terug naar hoofdmenu, maar ook spel?
+    public void Back()
+    {
+        //terug naar hoofdmenu scene
+        SoundManager.soundInstance.RandomizeSfx(buttonSound1, buttonSound2);
+        Application.LoadLevel(0);
+    }
+}
