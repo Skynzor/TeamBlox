9d257658070a748418c0ebfec5b88967a40b56d3
rikvanderzwaag <rikvanderzwaag@gmail.com>
2016-06-14 16:02:16 +0200

    Build3!

---
+++ INF2J_14_juni_FINAL_BUILD 3/TeamBloxio/Assets/Scripts/HighscoreMenu.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+
+public class HighscoreMenu : MonoBehaviour
+{
+
+    public List<Text> Texts;
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+        /* Test shizzl
+        PlayerPrefs.SetInt("highScore0", -5);
+        PlayerPrefs.SetString("highScoreName0", "test0");
+
+        PlayerPrefs.SetInt("highScore1", -10);
+        PlayerPrefs.SetString("highScoreName1", "test1");
+
+        PlayerPrefs.SetInt("highScore2", -30);
+        PlayerPrefs.SetString("highScoreName2", "test2");
+
+        PlayerPrefs.SetInt("highScore3", -20);
+        PlayerPrefs.SetString("highScoreName3", "test3");
+
+        PlayerPrefs.SetInt("highScore4", -50);
+        PlayerPrefs.SetString("highScoreName4", "test4");
+        
+
+        PlayerPrefs.Save();
+        Score.refreshHighscores();
+        */
+
+        Text[] highScores = new Text[5];
+        Text[] highScoresName = new Text[5];
+        for (int i = 0; i < 5; i++)
+        {
+            print(i.ToString());
+            highScores[i] = GameObject.Find("highScore" + i).GetComponent<Text>();
+            highScoresName[i] = GameObject.Find("highScoreName" + i).GetComponent<Text>();
+
+            highScores[i].text = PlayerPrefs.GetInt("highScore" + i, 0).ToString();
+            highScoresName[i].text = (i + 1).ToString() + ". " + PlayerPrefs.GetString("highScoreName" + i, "");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    //kan terug naar hoofdmenu, maar ook spel?
+    public void Back()
+    {
+        //terug naar hoofdmenu scene
+        Application.LoadLevel(0);
+    }
+}
---
+++ INF2J_14_juni_FINAL_BUILD 3/TeamBloxio/Assets/Scripts/Player1.cs
@@ -0,0 +1,158 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player1 : MonoBehaviour {
+
+    //Bepaal de maximum snelheid, snelheid en jumppower van de player
+    public float maxSpeed = 20;
+    public float speed = 300f;
+    public float jumpPower = 800f;
+
+    //Creëert een boolean om later te kunnen checken of de player op de grond staat
+    public bool grounded;
+
+    //Maak variabelen aan voor de rigidbody en de animation
+    private Rigidbody2D rb2d;
+    private Animator anim;
+
+    //Sound effects
+    public AudioClip jumpSound1;
+    public AudioClip jumpSound2;
+
+    void Start ()
+    {
+        rb2d = gameObject.GetComponent<Rigidbody2D>();
+        anim = gameObject.GetComponent<Animator>();
+        rb2d.mass = 0.5f; //Creëert de massa van de player
+        rb2d.gravityScale = 3f;
+        rb2d.constraints = RigidbodyConstraints2D.FreezeRotation;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //Verbind de variabele grounded met de functie "grounded" binnen Unity
+        //Hetzelfde geldt voor speed
+        anim.SetBool("grounded", grounded);
+        //anim.SetFloat("speed", Mathf.Abs(Input.GetAxis("Horizontal")));
+        anim.SetFloat("speed", Mathf.Abs(rb2d.velocity.x));
+
+        if (Input.GetAxis("Horizontal") < -0.1f)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+        if (Input.GetAxis("Horizontal") > 0.1f)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+
+        //if (Input.GetButtonDown("Jump") && grounded)
+        //{
+        //    rb2d.AddForce(Vector2.up * jumpPower);
+        //}
+
+        //Get de horizontale as
+        float h = Input.GetAxis("Horizontal");
+
+        //Check of de player op de grond staat
+        //Move player
+        //W A D (omhoog, links, rechts) 
+        if (grounded)
+        {
+            //rb2d.AddForce((Vector2.right * h) * speed);
+            if (Input.GetKey(KeyCode.A))
+            {
+                rb2d.AddForce((Vector3.right * -2.0f) * speed);
+                //Debug.Log("A ingedrukt");
+                //PlaySingle()
+                //musicSource.Stop()              
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                rb2d.AddForce((Vector2.right * 2.0f) * speed);
+                //Debug.Log("D ingedrukt");
+            }
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                rb2d.AddForce(Vector3.up * jumpPower);
+                //Debug.Log("W ingedrukt");
+                SoundManager.soundInstance.RandomizeSfx(jumpSound1, jumpSound2);
+
+            }
+        }
+        else
+        {
+            if (Input.GetKey(KeyCode.A))
+            {
+                rb2d.AddForce((Vector3.right * -1.0f) * speed);
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                rb2d.AddForce((Vector2.right * 1.0f) * speed);
+            }
+        }
+
+    }
+
+    //Een functie die je kunt maken die Unity automatisch aanroept
+    void FixedUpdate()
+    {
+        //Deze variabelen zorgen ervoor dat de player niet wegglijdt nadat het heeft bewogen/gesprongen
+        //Doe niets met de 'y' as
+        //Doe niets met de 'z' as (het is geen 3D, maar 2D)
+        //Geef de 'x' as een waarde onder 1 aan, zodat de player niet wegglijdt
+        Vector3 easeVelocity = rb2d.velocity;
+        easeVelocity.y = rb2d.velocity.y;
+        easeVelocity.z = 0.0f;
+        easeVelocity.x *= 0.75f;
+
+        
+
+        //Doe alsof er weerstand is en daarmee de snelheid van de player te verminderen
+        //Check of de player op de grond staat en voer dan de easeVelocity uit
+        if(grounded)
+        {
+            rb2d.velocity = easeVelocity;
+        }
+
+        
+
+        //Voeg een limiet toe aan de snelheid van de player
+        if(rb2d.velocity.x > maxSpeed)
+        {
+            rb2d.velocity = new Vector2(maxSpeed, rb2d.velocity.y);
+        }
+
+        if(rb2d.velocity.x < -maxSpeed)
+        {
+            rb2d.velocity = new Vector2(-maxSpeed, rb2d.velocity.y);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "Lava")
+        {
+            Destroy(gameObject);
+            Application.LoadLevel(Application.loadedLevel);
+        }
+        if (coll.gameObject.tag == "Enemy")
+        {
+            GameObject playerTag = GameObject.FindGameObjectWithTag("Player");
+            Destroy(playerTag);
+            Application.LoadLevel(Application.loadedLevel);
+        }
+        if (coll.transform.tag == "MovingPlatform")
+        {
+            //transform.parent = coll.transform;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D coll)
+    {
+        grounded = false;
+        //transform.parent = coll.transform;
+        gameObject.transform.parent = null;
+    }
+}
